2163b316785d2be283a65f9b7c0fdfca0687e3f9
2044295 <2044295@edison.k12.nj.us>
2019-07-22 15:53:51 -0400

    Add basic 'Hello World' app using express
    - Add code to perform 'Hello World' function
    - Add dependencies to package.json and package-lock.json
    - Add .gitignore to ignore node_modules

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 
-const PORT = process.env.PORT || 8888; // check to see if PORT variable is defined, otherwise use 8888
+const express = require('express');
 
-console.log('Hello World!');
-console.log(`Port: ${PORT}`);
+// Define PORT & create app
+const PORT = process.env.PORT || 8888; // check to see if PORT variable is defined, otherwise use 8888
+const app = express();
+
+// Define basic Hello World response
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Tell app to listen on appropriate PORT
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
