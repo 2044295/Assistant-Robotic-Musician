c8e37fa5b926a0ba92d8dbb32ebc9ae6a04bbb77
2044295 <2044295@edison.k12.nj.us>
2019-07-22 15:42:46 -0400

    Add code to find (and print) PORT #

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
+const PORT = process.env.PORT || 8888; // check to see if PORT variable is defined, otherwise use 8888
+
 console.log('Hello World!');
+console.log(`Port: ${PORT}`);
