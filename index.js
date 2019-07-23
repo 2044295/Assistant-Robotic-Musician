5cfcdfb8291a5bacadaa507988015fdf190149ca
2044295 <2044295@edison.k12.nj.us>
2019-07-22 20:41:11 -0400

    Add "ls" method for getting JSON list of files
    - Will be useful for a file explorer in the static /Projects directory
    - Uses a GET request, and the fs (file-stream) module
        - Requires path module for ease of functionality
    - Updates to package[-lock].json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const express = require('express');
+const path = require('path');
+const fs = require('file-system');
 
 // Define PORT & create app
 const PORT = process.env.PORT || 8888; // check to see if PORT variable is defined, otherwise use 8888
@@ -9,6 +11,64 @@ const app = express();
 // Define basic Hello World response
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// Define an "ls" method to list files in a directory
+app.get('/ls/*', (req, res) => {
+  // turn request URL into useful Path
+  var lsURL;
+  lsURL = req.url.split(path.sep);
+  lsURL = lsURL.slice(2, lsURL.length);
+
+  var lsPath = './';
+  lsURL.forEach(partial => {
+    lsPath += partial + '/';
+  });
+
+  console.log(`Files list request for ${lsPath}`);
+
+  // ensure that Path is legal to request
+  if (lsPath.slice(0, 11) !== './Projects/' && lsPath.slice(0, 9) !== './Public/') {
+    console.log(`Error: Request for ls of ${lsPath} is illegal!`);
+    res.status(500)
+      .jsonp({error: `You do not have permission to acces this path!`})
+      .end();
+    return 1;
+  }
+
+  // get list of files in requested directory
+  var filesObj = {
+    url: lsPath.slice(1, lsPath.length),
+    directories: [],
+    files: []
+  };
+
+  fs.readdir(lsPath, (err, dir) => {
+    // error handling
+    if (err) {
+      res.status(500).jsonp({error: err});
+      res.end();
+      return 1;
+    }
+
+    // processing files
+    dir.forEach(file => {
+      var relative = path.join(lsPath, file);
+      if (fs.lstatSync(relative).isDirectory()) {
+        filesObj.directories.push(file);
+      } else {
+        filesObj.files.push(file);
+      }
+    });
+
+    // send out found information
+    console.log(filesObj);
+    res.status(200)
+      .jsonp(filesObj)
+      .end();
+
+    return 0;
+  });
 });
 
 // Create the Static Path to Project Files
