61fe3d38a0ec2b608e6f126c48e3f303c5fbee3d
Evan Keeton <2044295@edison.k12.nj.us>
2019-08-12 14:47:41 -0400

    Include makeHTML.js in eslint.sh

--- eslint.sh
+++ eslint.sh
@@ -4,3 +4,4 @@ eslint --fix index.js
 eslint --fix Public/assets/scripts/file_explorer.js
 eslint --fix Private/ls/ls.js
 eslint --fix Private/respond/respond.js
+eslint --fix Private/respond/makeHTML.js
