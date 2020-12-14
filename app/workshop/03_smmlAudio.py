3abd1e3198b538a59a34c5abd10d11675373e78e
Evan Keeton <2044295@edison.k12.nj.us>
2020-12-14 08:20:46 -0500

    Add Experiments 2 and 3: Reading .wav files
    - Experiment 2: A simple demonstration (a single snapshot)
    - Experiment 3: Continuous frame sampling

--- app/workshop/03_smmlAudio.py
+++ app/workshop/03_smmlAudio.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import wave
 import audioop
+import time
 
 """"""
 """Setup: Use argparse to decide which examples to run"""
@@ -13,6 +14,14 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-1', '--sample_one', action='store_true',
                     help='Ex 1: Opening a .wav file')
+parser.add_argument('-2', '--sample_two', action='store_true',
+                    help='Ex 2: Reading a .wav file')
+parser.add_argument('-3', '--sample_three', action='store_true',
+                    help='Ex 3: Reading a .wav file in useful frames')
+parser.add_argument('--comment', action='store_true',
+                    help='Print non-JSON comments to the terminal')
+parser.add_argument('--overwrite', action='store_true',
+                    help='Overwrite printed data on continuous printing')
 
 argv = parser.parse_args(sys.argv[1:])
 
@@ -50,12 +59,56 @@ if argv.sample_one:
         results['open_write'] = str(sys.exc_info()[0])
 
     # open_write: testing opening with mode 'wb' (write-only)
+    # right now, this produces an error, but that's okay; we don't need to write
     try:
         with wave.open('Aud44.1k.wav', 'wb') as audio:
             results['open_write'] = ['opens write-only object', str(audio)]
     except:
         results['open_write'] = str(sys.exc_info()[0])
 
-    print(json.dumps('Example 1: Methods of Opening .wav files'))
+    if argv.comment: print('Example 1: Methods of Opening .wav files')
     print(json.dumps(results, indent=4))
     print()
+
+# Ex 2: Reading a .wav file
+if argv.sample_two:
+    results = {}
+    with wave.open('Aud8k.wav') as audio:
+        results['object'] = str(audio)
+        results['sample_width_bt'] = audio.getsampwidth()
+        results['sample_rate_hz'] = audio.getframerate()
+        results['sample_length_f'] = audio.getnframes()
+        results['sample_length_s'] = audio.getnframes()/audio.getframerate()
+        results['data'] = str(audio.readframes(30))
+
+    if argv.comment:
+        print('Example 2: Reading various .wav file data')
+        print('- Reading 30 bytes, for a short reference')
+    print(json.dumps(results, indent=2))
+    print()
+
+if argv.sample_three:
+    results = {}
+
+    if argv.comment:
+        print('Example 3: Reading a .wav file in useful frames')
+        print('- Reading 30 "frames per second" at 2 frames per second')
+        print('- 30 "frames per second" because A1 = 55hz')
+        print('- And printing 2 frames per second for the human mind')
+
+    with wave.open('Aud8k.wav') as audio:
+        results['object'] = str(audio)
+        results['sample_width_bt'] = audio.getsampwidth()
+        results['sample_rate_hz'] = audio.getframerate()
+        results['sample_length_f'] = audio.getnframes()
+        results['sample_length_s'] = audio.getnframes()/audio.getframerate()
+        results['frame_size'] = int(results['sample_rate_hz']/30)
+        results['frames'] = int(results['sample_length_f']/results['frame_size'])
+        for x in range(results['frames']):
+            results['data'] = str(audio.readframes(results['frame_size']))
+            print(json.dumps(results))
+            time.sleep(0.5)
+
+# WHERE TO GO FROM HERE:
+# - look into live input
+# - get into actual note recognition
