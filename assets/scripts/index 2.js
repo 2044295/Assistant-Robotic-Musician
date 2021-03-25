493d2047870be3789e879f2c32207ca34114aa39
Evan Keeton <2044295@edison.k12.nj.us>
2021-03-24 23:59:09 -0400

    Testing and debugging the frontend implementation

--- assets/scripts/index 2.js
+++
@@ -1,6 +0,0 @@
-const { ipcRenderer } = require('electron');
-function goHome() { ipcRenderer.send('load-page', 'home'); }
-function newFile() { ipcRenderer.send('load-page', 'newFile'); }
-function playPage() { ipcRenderer.send('load-page', 'playPage'); }
-function loadFile(filename) { ipcRenderer.send('loadFile', filename); }
-function trackFile(filename) { ipcRenderer.send('trackFile', filename); }
